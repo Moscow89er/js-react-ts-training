7b35f62d4138112841cde5a4e2f6ef58f878deac
Nikita Khubaev <khubaev.n@yandex.ru>
2023-07-20 14:14:13 +0400

    решение задачи на получение поля объекта

--- index.js
+++ index.js
@@ -44,75 +44,75 @@
 // Необходимо вывести среднее арифметическое по дням месяца в виде Map,
 // где будут дни месяца (в виде экземпляра Date),
 // а значениями средние величины
-const data = [
-    {
-        date: "2021-06-24T04:25:00.000Z",
-        value: 14
-    },
-    {
-        date: "2021-06-25T04:25:00.000Z",
-        value: 15
-    },
-    {
-        date: "2021-06-23T04:25:00.000Z",
-        value: 11
-    },
-    {
-        date: "2021-06-27T04:25:00.000Z",
-        value: 8
-    },
-    {
-        date: "2021-06-11T04:25:00.000Z",
-        value: 1
-    },
-    {
-        date: "2021-06-24T04:25:00.000Z",
-        value: 14
-    },
-    {
-        date: "2021-06-23T04:25:00.000Z",
-        value: 12
-    },
-    {
-        date: "2021-06-22T04:25:00.000Z",
-        value: 4
-    },
-    {
-        date: "2021-06-25T04:25:00.000Z",
-        value: 19
-    },
-    {
-        date: "2021-06-23T04:25:00.000Z",
-        value: 14
-    },
-    {
-        date: "2021-06-24T04:25:00.000Z",
-        value: 3
-    },
-    {
-        date: "2021-06-11T04:25:00.000Z",
-        value: 9
-    },
-];
+// const data = [
+//     {
+//         date: "2021-06-24T04:25:00.000Z",
+//         value: 14
+//     },
+//     {
+//         date: "2021-06-25T04:25:00.000Z",
+//         value: 15
+//     },
+//     {
+//         date: "2021-06-23T04:25:00.000Z",
+//         value: 11
+//     },
+//     {
+//         date: "2021-06-27T04:25:00.000Z",
+//         value: 8
+//     },
+//     {
+//         date: "2021-06-11T04:25:00.000Z",
+//         value: 1
+//     },
+//     {
+//         date: "2021-06-24T04:25:00.000Z",
+//         value: 14
+//     },
+//     {
+//         date: "2021-06-23T04:25:00.000Z",
+//         value: 12
+//     },
+//     {
+//         date: "2021-06-22T04:25:00.000Z",
+//         value: 4
+//     },
+//     {
+//         date: "2021-06-25T04:25:00.000Z",
+//         value: 19
+//     },
+//     {
+//         date: "2021-06-23T04:25:00.000Z",
+//         value: 14
+//     },
+//     {
+//         date: "2021-06-24T04:25:00.000Z",
+//         value: 3
+//     },
+//     {
+//         date: "2021-06-11T04:25:00.000Z",
+//         value: 9
+//     },
+// ];
 
-const result = data.reduce((acc, cur) => {
-    const date = new Date(cur.date).getTime();
+// const result = data.reduce((acc, cur) => {
+//     const date = new Date(cur.date).getTime();
 
-    if (acc.has(date)) {
-        acc.set(date, [...acc.get(date), cur.value]);
-    } else {
-        acc.set(date, [cur.value]);
-    }
-    return acc;
-}, new Map())
+//     if (acc.has(date)) {
+//         acc.set(date, [...acc.get(date), cur.value]);
+//     } else {
+//         acc.set(date, [cur.value]);
+//     }
+//     return acc;
+// }, new Map())
 
-const answer = new Map();
+// const answer = new Map();
 
-for (let [key, value] of result) {
-    answer.set(new Date(key), value.reduce((acc, cur) => acc + cur, 0) / value.length);
-}
+// for (let [key, value] of result) {
+//     answer.set(new Date(key), value.reduce((acc, cur) => acc + cur, 0) / value.length);
+// }
 
-console.log([...answer.entries()], answer);
+// console.log([...answer.entries()], answer);
 
 // let result = new Map();
 
@@ -136,3 +136,40 @@ console.log([...answer.entries()], answer);
 // }
 
 // console.log(result);
+
+// Нужно написать функцию get. На вход функция принимает объект и путь до поля объекта.
+// Путь это строка, разделенная точкой. Функция должна вернуть соответствующее поле объекта.
+// Запрашиваемого поля в объекте может не быть. 
+
+
+
+function get (obj, path) {
+    if (!path) return obj;
+
+    const paths = path.split('.');
+    let current = obj;
+
+    for (let i = 0; i < paths.length; i++) {
+        if (current[paths[i]] === undefined) {
+            return undefined;
+        }
+
+        current = current[paths[i]];
+    }
+
+    return current;
+}
+
+// Пример использования:
+const obj = {
+    a: {
+        b: {
+            c: 5
+        }
+    }
+};
+
+console.log(get(obj, 'a.b.c'));  // 5
+console.log(get(obj, 'a.b'));    // {c: 5}
+console.log(get(obj, 'a.b.d'));  // undefined
+console.log(get(obj, 'a.x.c'));  // undefined
