29c66a6590514ec0829b1ca6951f2f3077c6862c
Nikita Khubaev <khubaev.n@yandex.ru>
2023-10-29 14:27:03 +0400

    solved the getRandomMeaning task

---
+++ LEARN_JS/TASKS_FROM_MENTORS/getRandomMeaning.js
@@ -0,0 +1,21 @@
+// getRandomMeaning.js
+// 1)
+// let obj = {
+//     getRandomMeaning() {
+//         return Math.random() > 0.5
+//             ? null
+//             : 'Hurray!';
+//     }
+// }
+  
+// alert(obj.getRandomMeaning());
+
+// 2)
+// let obj = {
+//     randomMeaning: 
+//         Math.random() > 0.5 ?
+//         null :
+//         () => alert('Hurray!')
+// }
+
+// obj.randomMeaning?.();
