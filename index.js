4282befb86c2eccfa003dc929261973bc8bf3d1d
Nikita Khubaev <khubaev.n@yandex.ru>
2023-11-10 21:24:02 +0400

    practiced solving tasks from leetCode 2

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 // leatCode.js
 // Counter II
-var createCounter = function(init) {
-    let curVal = init;
+// var createCounter = function(init) {
+//     let curVal = init;
 
-    return {
-        increment() {
-            return curVal += 1;
-        },
-        decrement() {
-            return curVal -= 1;
-        },
-        reset() {
-            return curVal = init;
-        }
+//     return {
+//         increment() {
+//             return curVal += 1;
+//         },
+//         decrement() {
+//             return curVal -= 1;
+//         },
+//         reset() {
+//             return curVal = init;
+//         }
+//     }
+// };
+
+
+function sortDict(dict) {
+    let newArr = Object.entries(dict);
+
+    for (arr of newArr) {
+        arr[0] = isNaN(Number(arr[0])) ? arr[0] : Number(arr[0]);
     }
-};
+
+    return newArr.sort((a, b) => b[1] - a[1]);
+}
+
+console.log(sortDict({3:1, 2:2, 1:3}));
+console.log(sortDict({1:2, 2:4, 3:6}));
