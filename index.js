3796c2155cfccb3e282a9c1b02ab7d2412a81192
Nikita Khubaev <khubaev.n@yandex.ru>
2023-11-29 09:57:09 +0400

    practiced closure concepts again

--- index.js
+++ index.js
@@ -1,73 +1,22 @@
-// settimeoutSetinterval.js
+// closure.js
+// 1)
+function generateUniqId() {
+    let arrOfId = new Set();
 
-// const delay = 5000;
+    return function() {
+        let newId;
 
-// const timerId = setTimeout(function request() {
-//     // send request
-//     if (error because server overload) {
-//         delay *= 2;
-//     }
+        do {
+            newId = Math.floor(Math.random() * 1000)
+        } while (arrOfId.has(newId));
+        
+        arrOfId.add(newId);
 
-//     timerId = setTimeout(request, delay);
-// }, delay);
-
-// Минимальная задержка вложенных таймеров в браузере, пример
-// const start = Date.now();
-// const times = [];
-
-// setTimeout(function run() {
-//     times.push(Date.now() - start);
-
-//     if (start + 100 < Date.now()) alert(times);
-//     else setTimeout(run);
-// });
-
-function printNumbers(from, to) {
-    let current = from;
-
-    const intervalId = setInterval(()=> {
-        console.log(current);
-        if (current === to) {
-            clearInterval(intervalId);
-            return;
-        }
-        current++;
-    }, 1000);
-}
-
-// printNumbers(1, 5);
-
-function printNumbersByRecursion(from, to) {
-    let current = from;
-
-    const timerId = setTimeout(function repeat() {
-        console.log(current);
-        if (current === to) {
-            clearTimeout(timerId);
-            return;
-        }
-        current++;
-        setTimeout(repeat, 1000);
-    }, 1000);
-}
-
-// printNumbersByRecursion(1, 5);
-
-function printNumbersWithoutDelay(from, to) {
-    let current = from;
-
-    function go() {
-        console.log(current);
-        if (current === to) {
-            clearInterval(timerId);
-            return;
-        }
-        current++;
+        return newId;
     }
-
-    go();
-
-    const timerId = setInterval(go, 1000);
 }
 
-printNumbersWithoutDelay(1,5);
+const newId = generateUniqId();
+console.log(newId());
+console.log(newId());
+console.log(newId());
