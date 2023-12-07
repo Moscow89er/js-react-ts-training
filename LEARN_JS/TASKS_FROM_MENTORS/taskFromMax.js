7dc35349aa196564169bbf6b70c4196be059128e
Nikita Khubaev <khubaev.n@yandex.ru>
2023-12-07 21:40:06 +0400

    worked on memoizedFibonacci and [Symbol.iterator] concepts

--- LEARN_JS/TASKS_FROM_MENTORS/taskFromMax.js
+++ LEARN_JS/TASKS_FROM_MENTORS/taskFromMax.js
@@ -60,3 +60,121 @@ a.test = 123;
 
 console.log(a.test); // 123
 console.log(a._test); // 123
+
+// [Symbol.iterator] 06.12.2023
+// 1)
+let a = {
+  from: 1,
+  to: 5,
+
+	[Symbol.iterator]() {
+    return {
+      current: this.from,
+      last: this.to,
+
+      next() {
+        if (this.current <= this.last) {
+          return {
+            done: false,
+            value: this.current++
+          }
+        } else {
+          return {
+            done: true
+          }
+        }
+      }
+    }
+	},
+}
+
+console.log([...a]);
+
+// 2)
+const b = {
+  string1: "hello",
+  string2: "world",
+
+  [Symbol.iterator]() {
+    const combinedString = this.string1 + this.string2;
+    let index = 0;
+
+    return {
+      next() {
+        if (index < combinedString.length) {
+          return {
+            value: combinedString.charAt(index++),
+            done: false
+          }
+        } else { 
+          return {
+            done: true
+          }
+        }
+      }
+    }
+  }
+}
+
+console.log(...b);
+
+// 3)
+const d = {
+  [Symbol.iterator]() {
+    let step = 0;
+
+    return {
+      next() {
+        step++;
+        if (step === 1) {
+          return {
+            done: false,
+            value: "first value"
+          }
+        } else if (step === 2) {
+          return {
+            done: false,
+            value: "second value"
+          }
+        } else {
+          return {
+            done: true
+          }
+        }
+      }
+    }
+  }
+}
+
+const [t1, t2] = d;
+console.log(t1, t2);
+
+// 4)
+const range = {
+  start: 1,
+  end: 5
+};
+
+range[Symbol.iterator] = function() {
+    let current = this.start;
+    let last = this.end;
+    
+    return {
+      next() {
+        if (current <= last) {
+          return {
+            done: false,
+            value: current++
+          }
+        } else {
+          return {
+            done: true
+          }
+        }
+      }
+    }
+}
+
+for (let num of range) {
+  console.log(num);
+}
--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-// iteration.js
-// 1)
-let a = {
-	[Symbol.iterator]() {
-    return {
-      next() {
-        if (this.current) {
-          return {
-            done: false,
-            value: this.current++
-          }
-        } else {
-          return {
-            done: true
-          }
-        }
-      }
+function memoizedFibonacci() {
+  let cache = {};
+
+  function fib(n) {
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+
+    if (cache[n]) {
+      return cache[n];
     }
-	},
+
+    const result = fib(n - 1) + fib(n - 2);
+    cache[n] = result;
+    return result;
+  }
+
+  return fib;
 }
 
-console.log([...a]);
+const fibonacci = memoizedFibonacci();
+
+console.log(fibonacci(6));
+console.log(fibonacci(7));
+console.log(fibonacci(1000));
