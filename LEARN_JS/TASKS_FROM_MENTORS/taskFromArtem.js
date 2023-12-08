1a15a353b0b8e1387c9bfc4461f0efdd9a827413
Nikita Khubaev <khubaev.n@yandex.ru>
2023-12-08 10:19:58 +0400

    working on tasks from mentors (apply, call, bind)

--- LEARN_JS/TASKS_FROM_MENTORS/taskFromArtem.js
+++ LEARN_JS/TASKS_FROM_MENTORS/taskFromArtem.js
@@ -1,4 +1,4 @@
-// Задача от Артема (Catana)
+// Задача от Артема (Katana)
 const {0: one, 1: two} = [1, 2];
 
 console.log(one);
@@ -31,3 +31,74 @@ console.log(three);
 console.log(four);
 console.log(five);
 console.log(six);
+
+// ОС от Артема 03.12
+// 1)
+const obj = {
+    name: "foo",
+  
+    bar() {
+      return this.name;
+    },
+  
+    baz: () => {
+      return this.name;
+    }
+  }
+  
+  const obj2 = {
+    name: "faz"
+  }
+  
+  console.log(obj.bar());
+  console.log(obj.baz());
+  
+  console.log(obj.bar.call(obj2));
+  console.log(obj.baz.call(obj2));
+  
+  console.log(obj.bar.bind(obj2)());
+  console.log(obj.baz.bind(obj2)());
+
+  // 2)
+const person = {
+    name: 'Alice',
+    age: 25,
+    introduce() {
+      console.log(`Меня зовут ${this.name} и мне ${this.age} лет.`);
+    }
+  };
+  
+  const person2 = {
+    name: 'Bob',
+    age: 30
+  };
+  
+  person.introduce.call(person2);
+  
+  // 3)
+  function printWithArguments(message) {
+    for(let i = 1; i < arguments.length; i++) {
+      console.log(`${message}: ${arguments[i]}`);
+    }
+  };
+  
+  const stringObj = {
+    message: "Слово 'Внимание' на разных языках"
+  };
+  
+  printWithArguments.apply(stringObj, [stringObj.message, "Attention! - английский", "ყურადღება! - грузинский"]);
+  
+  // 4)
+  let user = {
+    name: 'Sarah'
+  };
+  
+  function updateName(newName) {
+    this.name = newName;
+  }
+  
+  const giveSarahNewName = updateName.bind(user);
+  
+  giveSarahNewName("Nina");
+  
+  console.log(user.name);
