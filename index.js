88f451b2ca05d6f8e6a9fa866365cb664a8937bf
Nikita Khubaev <khubaev.n@yandex.ru>
2023-12-12 11:40:59 +0400

    practiced Class ES6 concepts 2

--- index.js
+++ index.js
@@ -53,31 +53,53 @@ class Clock {
     };
 }
 
+class ExtendedClock extends Clock {
+    constructor(options) {
+        super(options);
+        const {precision = 1000 } = options;
+        this.precision = precision;
+    }
+
+    start() {
+        this.render();
+        this.timer = setInterval(() => this.render(), this.precision);
+    }
+}
+
 const clock = new Clock({ template: 'h:m:s' });
-clock.start();
-setTimeout(() => clock.stop(), 5000);
+// clock.start();
+// setTimeout(() => clock.stop(), 5000);
+
+const extendedClock = new ExtendedClock({ template: 'h:m:s', precision: 2000 });
+// extendedClock.start();
+// setTimeout(() => extendedClock.stop(), 5000);
+
 
 // 2)
 class Animal {
-    constructor(name) {
-        this.speed = 0;
+    constructor(name, speed) {
+        this.speed = speed;
         this.name = name;
     }
-    run(speed) {
-        this.speed = speed;
+    run(speed = 0) {
+        this.speed += speed;
         console.log(`${this.name} runs with the speed ${this.speed}.`);
     }
     stop() {
         this.speed = 0;
         console.log(`${this.name} stands motionless.`);
     }
+
+    static compare(animalA, animalB) {
+        return animalA.speed - animalB.speed;
+    }
 }
 
-const animal = new Animal("My pet");
+const animal = new Animal("My pet", 0);
 
 class Rabbit extends Animal {
-    constructor(name, earLength) {
-        super(name);
+    constructor(name, earLength, speed) {
+        super(name, speed);
         this.earLength = earLength;
     }
 
@@ -91,10 +113,199 @@ class Rabbit extends Animal {
     }
 }
 
-const rabbit = new Rabbit("White rabbit", 10);
+const rabbits = [
+    new Rabbit("White rabbit", 10, 5),
+    new Rabbit("Black rabbit", 5, 3)
+];
 
-rabbit.run(5);
-rabbit.stop();
+// rabbits.sort(Rabbit.compare);
 
-console.log(rabbit.name);
-console.log(rabbit.earLength);
+// rabbits[0].run();
+
+// 3)
+const brute = {
+    name: "Animal",
+    eat() { // brute.eat.[[HomeObject]] == brute
+        console.log(`${this.name} eats`);
+    }
+}
+
+const raccoon = {
+    __proto__: brute,
+    name: "Raccoon",
+    eat() { // raccoon.eat.[[HomeObject]] == raccoon
+        super.eat();
+    }
+}
+
+const longTail = {
+    __proto__: raccoon,
+    name: "longTail",
+    eat() { // longTail.eat.[[HomeObject]] == longTail
+        super.eat();
+    }
+}
+
+// longTail.eat();
+
+// 4)
+class Animal1 {
+    constructor(name) {
+      this.name = name;
+    }
+  
+}
+  
+class Rabbit1 extends Animal1 {
+    constructor(name) {
+        super(name);
+        this.created = Date.now();
+    }
+}
+  
+const rabbit1 = new Rabbit1("Белый кролик");
+// console.log(rabbit.name);
+
+// 4)
+class Rabbit3 extends Object {
+    constructor(name) {
+        super();
+        // Унаследованный конструктор класса должен вызывать super().
+        // В противном случае "this" будет не определён.
+        this.name = name;
+    }
+}
+  
+const rabbit = new Rabbit3("Кроль");
+  
+// console.log( rabbit.hasOwnProperty('name') );
+
+// 5) Создание кофеварки
+class CoffeeMachine {
+    _waterAmount = 0;
+    #waterLimit = 200;
+
+    constructor(power) {
+        this._power = power;
+        console.log(`Создана кофеварка, мощность: ${power}`);
+    }
+
+    #checkWater(value) {
+        if (value < 0) throw new Error("Отрицательное количество воды");
+        if (value > this.#waterLimit) throw new Error("Слишком много воды");
+    }
+
+    setWaterAmount(value) {
+        this.#checkWater(value);
+        this._waterAmount = value;
+    }
+
+    getWaterAmount() {
+        return this._waterAmount;
+    }
+
+    getPower() {
+        return this._power;
+    }
+}
+
+const coffeeMachine = new CoffeeMachine(150);
+
+coffeeMachine.setWaterAmount(175);
+
+console.log(coffeeMachine);
+
+// 6)
+class PowerArray extends Array {
+    isEmpty() {
+        return this.length === 0;
+    }
+
+    // встроенные методы массива будут использовать этот
+    // метод как конструктор
+    // static get [Symbol.species]() {
+    //     return Array;
+    // }
+}
+
+const arr = new PowerArray(1, 2, 5, 10, 50);
+console.log(arr.isEmpty());
+
+const filteredArr = arr.filter(item => item >= 10);
+console.log(filteredArr);
+console.log(filteredArr.isEmpty());
+
+// 7) mixins
+const sayMixin = {
+    say(phrase) {
+        console.log(phrase);
+    }
+}
+
+const sayHiMixin = {
+    __proto__: sayMixin,
+
+    sayHi() {
+        super.say(`Привет, ${this.name}`);
+    },
+    sayBye() {
+        super.say(`Пока, ${this.name}`);
+    }
+}
+
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+Object.assign(User.prototype, sayHiMixin);
+
+new User("Nick").sayBye();
+
+// 8) EventMixin
+const eventMixin = {
+    on(eventName, handler) {
+        if (!this._eventHandlers) this._eventHandlers = {};
+
+        if (!this._eventHandlers[eventName]) {
+            this._eventHandlers[eventName] = [];
+        }
+
+        this._eventHandlers[eventName].push(handler);
+    },
+
+    off(eventName, handler) {
+        const handlers = this._eventHandlers?.[eventName];
+
+        if (!handlers) return;
+
+        for (let i = 0; i < handlers.length; i++) {
+            if(handlers[i] === handler) {
+                handlers.splice(i--, 1)
+            }
+        }
+    },
+
+    trigger(eventName, ...args) {
+        if (!this._eventHandlers?.[eventName]) {
+            return;
+        }
+
+        this._eventHandlers[eventName].forEach(handler => handler.apply(this, args));
+    }
+}
+
+class Menu {
+    choose(value) {
+        this.trigger("select", value);
+    }
+}
+
+Object.assign(Menu.prototype, eventMixin);
+
+const menu = new Menu();
+
+menu.on("select", value => console.log(`Выбранное значение: ${value}`));
+
+menu.choose("Жаренная селедочка по-флотски");
