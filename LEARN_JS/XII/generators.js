2ee923baa7c239b37bfb470fec3be0f31d1805e3
Nikita Khubaev <khubaev.n@yandex.ru>
2023-12-29 09:10:07 +0400

    practiced generator concepts 2

---
+++ LEARN_JS/XII/generators.js
@@ -0,0 +1,115 @@
+// 1)
+const range = {
+    from: 1,
+    to: 5,
+
+    *[Symbol.iterator]() { // краткая запись для [Symbol.iterator]: function*()
+        for(let value = this.from; value <= this.to; value++) {
+            yield value;
+        }
+    }
+}
+
+console.log([...range]);
+
+// 2) Композиция генераторов
+
+function* generateSequence(start, end) {
+    for (let i = start; i <= end; i++) yield i;
+}
+
+// a)
+function* generatePasswordCodes() {
+    // 0...9
+    yield* generateSequence(48, 57);
+
+    // A...Z
+    yield* generateSequence(65, 90);
+
+    // a...z
+    yield* generateSequence(97, 122);
+}
+
+// b)
+function* generateAlphaNum() {
+    // yield* generateSequence(48, 57);
+    for (let i = 48; i <= 57; i++) yield i;
+
+    // yield* generateSequence(65, 90);
+    for (let i = 65; i <= 90; i++) yield i;
+
+    // yield* generateSequence(97, 122);
+    for (let i = 97; i <= 122; i++) yield i;
+}
+
+let str = "";
+let str2 = "";
+
+for (let code of generatePasswordCodes()) {
+    str += String.fromCharCode(code);
+}
+
+for(let code of generateAlphaNum()) {
+    str2 += String.fromCharCode(code);
+}
+
+console.log(str);
+console.log(str2);
+
+// 3) yield – дорога в обе стороны:
+// он не только возвращает результат наружу,
+// но и может передавать значение извне в генератор.
+
+function* gen() {
+    // Передаём вопрос во внешний код и ожидаем ответа
+    const result = yield "2 + 2 = ?";
+    console.log(result); // 4
+
+    const result2 = yield "3 * 3 = ?";
+    console.log(result2); // 9
+}
+
+const generator = gen();
+const question = generator.next().value; // yield возвращает значение
+
+// a)
+generator.next(4); // передаем результат в генератор
+// b)
+setTimeout(() => generator.next(9), 2000); // возобновить генератор через некоторое время
+
+setTimeout(() => console.log(generator.next(9).done), 2500); // true
+
+// 4) generator.throw: можно передать не только результат,
+// но и инициировать ошибку. Это естественно,
+// так как ошибка является своего рода результатом.
+
+function* gen() {
+    try {
+        const result = yield "2 + 2 = ?";
+
+        console.log("Выполнение программы не дойдёт до этой строки, потому что выше возникнет исключение");
+    } catch(e) {
+        console.log(e); // покажет ошибку
+    }
+}
+
+const generator2 = gen();
+const question2 = generator2.next().value;
+
+generator2.throw(new Error("Ответ не найден в моей базе данных"));
+
+// 5)
+function* pseudoRandom(seed) {
+    let previous = seed;
+
+    while (true) {
+        previous = previous * 16807 % 2147483647;
+        yield previous;
+    }
+}
+
+let generator3 = pseudoRandom(1);
+
+// console.log(generator3.next().value);
+// console.log(generator3.next().value);
+// console.log(generator3.next().value);
--- index.js
+++ index.js
@@ -1,100 +1,40 @@
 // generators.js
-// 1)
-const range = {
+
+
+// asyncIteratorsGenerators.js
+const asyncRange = {
     from: 1,
     to: 5,
-
-    *[Symbol.iterator]() { // краткая запись для [Symbol.iterator]: function*()
-        for(let value = this.from; value <= this.to; value++) {
-            yield value;
+  
+    // for await..of вызывает этот метод один раз в самом начале
+    [Symbol.asyncIterator]() {
+      // ...возвращает объект-итератор:
+      // далее for await..of работает только с этим объектом,
+      // запрашивая у него следующие значения вызовом next()
+      return {
+        current: this.from,
+        last: this.to,
+  
+        // next() вызывается на каждой итерации цикла for await..of
+        async next() {
+          // должен возвращать значение как объект {done:.., value :...}
+          // (автоматически оборачивается в промис с помощью async)
+  
+          // можно использовать await внутри для асинхронности:
+          await new Promise(resolve => setTimeout(resolve, 1000));
+  
+          if (this.current <= this.last) {
+            return { done: false, value: this.current++ };
+          } else {
+            return { done: true };
+          }
         }
+      };
     }
-}
+  };
 
-console.log([...range]);
-
-// 2) Композиция генераторов
-
-function* generateSequence(start, end) {
-    for (let i = start; i <= end; i++) yield i;
-}
-
-// a)
-function* generatePasswordCodes() {
-    // 0...9
-    yield* generateSequence(48, 57);
-
-    // A...Z
-    yield* generateSequence(65, 90);
-
-    // a...z
-    yield* generateSequence(97, 122);
-}
-
-// b)
-function* generateAlphaNum() {
-    // yield* generateSequence(48, 57);
-    for (let i = 48; i <= 57; i++) yield i;
-
-    // yield* generateSequence(65, 90);
-    for (let i = 65; i <= 90; i++) yield i;
-
-    // yield* generateSequence(97, 122);
-    for (let i = 97; i <= 122; i++) yield i;
-}
-
-let str = "";
-let str2 = "";
-
-for (let code of generatePasswordCodes()) {
-    str += String.fromCharCode(code);
-}
-
-for(let code of generateAlphaNum()) {
-    str2 += String.fromCharCode(code);
-}
-
-console.log(str);
-console.log(str2);
-
-// 3) yield – дорога в обе стороны:
-// он не только возвращает результат наружу,
-// но и может передавать значение извне в генератор.
-
-function* gen() {
-    // Передаём вопрос во внешний код и ожидаем ответа
-    const result = yield "2 + 2 = ?";
-    console.log(result); // 4
-
-    const result2 = yield "3 * 3 = ?";
-    console.log(result2); // 9
-}
-
-const generator = gen();
-const question = generator.next().value; // yield возвращает значение
-
-// a)
-generator.next(4); // передаем результат в генератор
-// b)
-setTimeout(() => generator.next(9), 2000); // возобновить генератор через некоторое время
-
-setTimeout(() => console.log(generator.next(9).done), 2500); // true
-
-// 4) generator.throw: можно передать не только результат,
-// но и инициировать ошибку. Это естественно,
-// так как ошибка является своего рода результатом.
-
-function* gen() {
-    try {
-        const result = yield "2 + 2 = ?";
-
-        console.log("Выполнение программы не дойдёт до этой строки, потому что выше возникнет исключение");
-    } catch(e) {
-        console.log(e); // покажет ошибку
+(async () => {
+    for await (let value of asyncRange) {
+        console.log(value);
     }
-}
-
-const generator2 = gen();
-const question2 = generator2.next().value;
-
-generator2.throw(new Error("Ответ не найден в моей базе данных"));
+})()
