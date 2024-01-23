2f09756dff8b55f6e74548b1861805f1ff2b572a
Nikita Khubaev <khubaev.n@yandex.ru>
2024-01-23 09:58:44 +0400

    practiced working with DOM and document concepts 3

--- index.js
+++ index.js
@@ -90,3 +90,30 @@ function createWatch() {
   startWatch();
   stopWatch();
 }
+
+// 3)
+function showNotification(options) {
+  let notification = document.createElement("div");
+  notification.classList.add("notification");
+  
+  notification.style.marginTop = options.top + "px";
+  notification.style.marginRight = options.right + "px";
+  notification.textContent = options.html;
+
+  if (options.className) {
+    notification.classList.add(options.className);
+  }
+
+  document.body.append(notification);
+
+  setTimeout(() => {
+    notification.remove();
+  }, 1500);
+}
+
+showNotification({
+  top: 10, // 10px от верхней границы окна (по умолчанию 0px)
+  right: 10, // 10px от правого края окна (по умолчанию 0px)
+  html: "Hello!", // HTML-уведомление
+  className: "welcome" // дополнительный класс для div (необязательно)
+});
