ccef0aba1ceb84029e4d8ed95c0bb9804c030e90
Nikita Khubaev <khubaev.n@yandex.ru>
2024-01-23 19:29:12 +0400

    practiced working with DOM and document concepts 5

--- index.js
+++ index.js
@@ -138,3 +138,12 @@ function getScrollBarWidth() {
 }
 
 getScrollBarWidth();
+
+// 6)
+function putBallInCenterWithoutCss() {
+  const ball = document.querySelector("#ball");
+  const field = document.querySelector("#field");
+
+  ball.style.left = Math.round(field.clientWidth / 2 - ball.offsetWidth / 2) + "px";
+  ball.style.top = Math.round(field.clientHeight / 2 - ball.offsetHeight / 2) + "px";
+}
