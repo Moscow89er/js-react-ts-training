2a09cf5f5d127ea6be8c04878d41eb084beb4493
Nikita Khubaev <khubaev.n@yandex.ru>
2024-02-01 20:57:22 +0400

    practiced with LeetCode tasks

--- FREE_PRACTICE/codeTraining2.js
+++ FREE_PRACTICE/codeTraining2.js
@@ -743,3 +743,94 @@
 // }
 
 // const memoizedFactorial = memoized(n => factorial(n, memoizedFactorial));
+
+// // 1)
+// function incapsulatedRemapData() {
+//     const data = [
+//         ['Tomato', 'Nick', 'Igor'],
+//         ['Cucumber', 'Ivan', 'Igor'],
+//         ['Rice', 'Nick'],
+//         ['Eggs', 'Ivan', 'Nick']
+//       ]
+      
+//       function remapData(data) {
+//         return data.reduce((acc, arr) => {
+//           let product = arr[0];
+      
+//           for (let i = 1; i < arr.length; i++) {
+//             if (acc[arr[i]]) {
+//               acc[arr[i]].push(product);
+//             } else {
+//               acc[arr[i]] = [product];
+//             }
+//           }
+//           return acc;
+//         }, {})
+//       }
+      
+//       console.log(remapData(data));
+// }
+
+// // incapsulatedRemapData();
+
+// // 2)
+// function calculateAverageValuesByDate() {
+//     const data = [/* ... ваш массив данных ... */];
+
+//     function remapData(data) {
+//     // Преобразуем массив данных в Map, группируя по датам
+//     const groupedByDate = data.reduce((accumulator, currentItem) => {
+//     // Получаем дату и значение из текущего элемента
+//         const date = new Date(currentItem.date).toDateString(); // Преобразуем дату в строку для уникальности
+//         const value = currentItem.value;
+
+//         // Добавляем значение в массив для соответствующей даты
+//         if (accumulator.has(date)) {
+//             accumulator.get(date).push(value);
+//         } else {
+//             accumulator.set(date, [value]);
+//         }
+
+//         return accumulator;
+//     }, new Map());
+
+//     // Создаем Map для ответа, содержащего средние значения
+//     const answer = new Map();
+//         for (let [date, values] of groupedByDate) {
+//             // Рассчитываем среднее значение для каждой даты
+//             const average = values.reduce((sum, current) => sum + current, 0) / values.length;
+//             answer.set(date, average);
+//         }
+
+//         return answer;
+//     }
+
+//     const result = remapData(data);
+//     console.log(Array.from(result.entries()), result);
+// }
+
+// // 3)
+// class Test {
+//   #privateMethod() {
+// 		console.log(1);
+// 	}
+
+//   publicMethod() {
+//     console.log(2);
+//     this.#privateMethod(); // Приватный метод может быть вызван внутри класса
+//   }
+
+// 	static staticMethod() {
+// 		console.log('Вызов статического метода');
+// 	}
+// }
+
+// // Test.privateMethod(); // Это вызовет ошибку, так как метод приватный
+
+// const newTest = new Test();
+
+// // newTest.privateMethod(); // Это также вызовет ошибку, так как метод приватный
+
+// newTest.publicMethod(); // Выведет 2 и затем 1, так как publicMethod вызывает privateMethod внутри класса
+
+// Test.staticMethod(); // Выведет 'Вызов статического метода'
--- index.js
+++ index.js
@@ -1,90 +1,60 @@
+// leatcode.js
 // 1)
-function incapsulatedRemapData() {
-    const data = [
-        ['Tomato', 'Nick', 'Igor'],
-        ['Cucumber', 'Ivan', 'Igor'],
-        ['Rice', 'Nick'],
-        ['Eggs', 'Ivan', 'Nick']
-      ]
-      
-      function remapData(data) {
-        return data.reduce((acc, arr) => {
-          let product = arr[0];
-      
-          for (let i = 1; i < arr.length; i++) {
-            if (acc[arr[i]]) {
-              acc[arr[i]].push(product);
-            } else {
-              acc[arr[i]] = [product];
-            }
-          }
-          return acc;
-        }, {})
-      }
-      
-      console.log(remapData(data));
+function reduce() {
+  nums = [1,2,3,4]
+  fn = function sum(accum, curr) { return accum + curr; }
+  init = 0
+  
+  var reduce = function(nums, fn, init) {
+    if (nums.length === 0) return init;
+  
+    for (let i = 0; i < nums.length; i++) {
+      init = fn(init, nums[i]);
+    }
+  
+    return init;
+  };
+  
+  console.log(reduce(nums, fn, init));
 }
 
-// incapsulatedRemapData();
+// reduce();
 
 // 2)
-function calculateAverageValuesByDate() {
-    const data = [/* ... ваш массив данных ... */];
+function incapsulatedCompose() {
+  const functions = [x => x + 1, x => x * x, x => 2 * x];
+  const x = 4;
 
-    function remapData(data) {
-    // Преобразуем массив данных в Map, группируя по датам
-    const groupedByDate = data.reduce((accumulator, currentItem) => {
-    // Получаем дату и значение из текущего элемента
-        const date = new Date(currentItem.date).toDateString(); // Преобразуем дату в строку для уникальности
-        const value = currentItem.value;
+  var compose = function(functions) {
+    return function(x) {
+       if (functions.length === 0) return x;
 
-        // Добавляем значение в массив для соответствующей даты
-        if (accumulator.has(date)) {
-            accumulator.get(date).push(value);
-        } else {
-            accumulator.set(date, [value]);
-        }
+       let result = x;
+  
+       for (let i = 0; i < functions.length; i++) {
+        result = functions[i](result);
+       }
 
-        return accumulator;
-    }, new Map());
-
-    // Создаем Map для ответа, содержащего средние значения
-    const answer = new Map();
-        for (let [date, values] of groupedByDate) {
-            // Рассчитываем среднее значение для каждой даты
-            const average = values.reduce((sum, current) => sum + current, 0) / values.length;
-            answer.set(date, average);
-        }
-
-        return answer;
+       return result;
     }
+  };
 
-    const result = remapData(data);
-    console.log(Array.from(result.entries()), result);
+  // from right to left
+  // var compose = function(functions) { 
+  //   return function(x) {
+  //     if (functions.length === 0) return x;
+
+  //     let result = x;
+ 
+  //     for (let i = functions.length - 1; i >= 0; i--) {
+  //      result = functions[i](result);
+  //     }
+
+  //     return result;
+  //   }
+  // };
+
+  console.log(compose(functions)(x));
 }
 
-// 3)
-class Test {
-  #privateMethod() {
-		console.log(1);
-	}
-
-  publicMethod() {
-    console.log(2);
-    this.#privateMethod(); // Приватный метод может быть вызван внутри класса
-  }
-
-	static staticMethod() {
-		console.log('Вызов статического метода');
-	}
-}
-
-// Test.privateMethod(); // Это вызовет ошибку, так как метод приватный
-
-const newTest = new Test();
-
-// newTest.privateMethod(); // Это также вызовет ошибку, так как метод приватный
-
-newTest.publicMethod(); // Выведет 2 и затем 1, так как publicMethod вызывает privateMethod внутри класса
-
-Test.staticMethod(); // Выведет 'Вызов статического метода'
+incapsulatedCompose();
